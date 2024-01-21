7feec89561f725192956b5d15f35dee7c391ba7d
prasanth4a5 <ummadi1994@gmail.com>
2024-01-21 19:40:55 +0530

    changes

--- main.tf
+++ main.tf
@@ -37,16 +37,16 @@ resource "aws_security_group" "main" {
   }
 }
 
-resource "aws_security_group_rule" "nginx_exporter" {
-  count             = var.component == "frontend" ? 1 : 0
-  type              = "ingress"
-  from_port         = 9113
-  to_port           = 9113
-  protocol          = "tcp"
-  cidr_blocks       = var.monitoring_ingress_cidr
-  security_group_id = aws_security_group.main.id
-  description       = "Nginx Prometheus Exporter"
-}
+#resource "aws_security_group_rule" "nginx_exporter" {
+#  count             = var.component == "frontend" ? 1 : 0
+#  type              = "ingress"
+#  from_port         = 9113
+#  to_port           = 9113
+#  protocol          = "tcp"
+#  cidr_blocks       = var.monitoring_ingress_cidr
+#  security_group_id = aws_security_group.main.id
+#  description       = "Nginx Prometheus Exporter"
+#}
 
 resource "aws_iam_policy" "main" {
   name        = "${local.name_prefix}-policy"
@@ -250,20 +250,20 @@ resource "aws_lb_target_group_attachment" "public" {
   availability_zone = "all"
 }
 
-
-resource "aws_lb_listener_rule" "public" {
-  count        = var.component == "frontend" ? 1 : 0
-  listener_arn = var.public_listener
-  priority     = var.lb_priority
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.public[0].arn
-  }
-
-#  condition {
-#    host_header {
-#      values = ["${var.env == "prod" ? "www" : var.env}.prashdevops.online"]
-#    }
+#
+#resource "aws_lb_listener_rule" "public" {
+#  count        = var.component == "frontend" ? 1 : 0
+#  listener_arn = var.public_listener
+#  priority     = var.lb_priority
+#
+#  action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.public[0].arn
 #  }
-}
+#
+##  condition {
+##    host_header {
+##      values = ["${var.env == "prod" ? "www" : var.env}.prashdevops.online"]
+##    }
+##  }
+#}
