feca10baea4f2ec8483088ac08ad79a117e614d3
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-02-05 14:50:26 +0530

    Test

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-
-ansible-pull -i localhost, -U https://github.com/Prasanthreddy4a5/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
-
+##!/bin/bash
+#
+#ansible-pull -i localhost, -U https://github.com/Prasanthreddy4a5/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
+#
