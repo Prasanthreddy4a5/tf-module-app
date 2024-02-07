3d811ea154cf40e7907fda5d5216fef3a221b994
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-02-07 14:10:26 +0530

    Test

--- data.tf
+++ data.tf
@@ -1,16 +1,16 @@
-##data "aws_ami" "ami" {
-##  most_recent = true
-##  name_regex  = "Centos-8-DevOps-Practice"
-##  owners      = ["973714476881"]
-##}
-#
-#data "aws_ami" "ami" {
-#  most_recent = true
-#  name_regex  = "roboshop-ami-v1"
-#  owners      = ["633788536644"]
-#}
-#
-#
-#data "dns_a_record_set" "private_alb" {
-#  host = var.private_alb_name
-#}
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "roboshop-ami-v1"
+  owners      = ["633788536644"]
+}
+
+
+data "dns_a_record_set" "private_alb" {
+  host = var.private_alb_name
+}
