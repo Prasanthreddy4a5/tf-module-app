12e5587b000536cee119df6f1fe182bc267218b9
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-02-08 15:54:01 +0530

    Test

--- main.tf
+++ main.tf
@@ -178,47 +178,52 @@ resource "aws_autoscaling_group" "main" {
 #  }
 #}
 
-#resource "aws_route53_record" "main" {
-#  zone_id = var.zone_id
-#  name    = var.component == "frontend" ? var.env == "prod" ? "www" : var.env : "${var.component}-${var.env}"
-#  type    = "CNAME"
-#  ttl     = 30
-#  records = [var.component == "frontend" ? var.public_alb_name : var.private_alb_name]
-#}
+resource "aws_route53_record" "main" {
+  zone_id = var.zone_id
+  name    = var.component == "frontend" ? var.env == "prod" ? "www" : var.env : "${var.component}-${var.env}"
+  type    = "CNAME"
+  ttl     = 30
+  records = [var.component == "frontend" ? var.public_alb_name : var.private_alb_name]
+}
 
 resource "aws_lb_target_group" "main" {
   name     = local.name_prefix
   port     = var.port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-  deregistration_delay = 15
-  health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    interval            = 5
-    path                = "/health"
-    port                = var.port
-    timeout             = 2
-    unhealthy_threshold = 2
-    matcher             = "200"
-  }
+#  deregistration_delay = 15
+#  health_check {
+#    enabled             = true
+#    healthy_threshold   = 2
+#    interval            = 5
+#    path                = "/health"
+#    port                = var.port
+#    timeout             = 2
+#    unhealthy_threshold = 2
+#    matcher             = "200"
+#  }
 }
 
-#resource "aws_lb_listener_rule" "main" {
-#  listener_arn = var.private_listener
-#  priority     = var.lb_priority
-#
-#  action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.main.arn
-#  }
-#
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = var.private_listener
+  priority     = var.lb_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
 #  condition {
 #    host_header {
 #      values = [var.component == "frontend" ? "${var.env == "prod" ? "www" : var.env}.prashdevops.online" : "${var.component}-${var.env}.prashdevops.online"]
 #    }
-#  }
-#}
+  condition {
+    host_header {
+      values = [var.component == "frontend" ? "${var.env}.prashdevops.online" : "${var.component}-${var.env}.prashdevops.online.online"]
+    }
+
+  }
+}
 
 resource "aws_lb_target_group" "public" {
   count       = var.component == "frontend" ? 1 : 0
@@ -228,16 +233,16 @@ resource "aws_lb_target_group" "public" {
   protocol    = "HTTP"
   vpc_id      = var.default_vpc_id
 
-  health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    interval            = 5
-    path                = "/"
-    port                = var.port
-    timeout             = 2
-    unhealthy_threshold = 2
-    matcher             = "404"
-  }
+#  health_check {
+#    enabled             = true
+#    healthy_threshold   = 2
+#    interval            = 5
+#    path                = "/"
+#    port                = var.port
+#    timeout             = 2
+#    unhealthy_threshold = 2
+#    matcher             = "404"
+#  }
 
 }
 
